03fa182067ba410b8f1aa384281aa509c504ac05
sda97ghb <sda97g@gmail.com>
2016-12-08 01:36:03 +0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <time.h>
+
+static constexpr int NUMBER_OF_EXPERIMENTS = 1000000;
+static constexpr bool CHANGE_DOOR = false;
+
+// Choose random element from the set
+int chooseRandom(std::vector<int> numbers)
+{
+    int n = rand() % numbers.size();
+    return numbers[n];
+}
+
+// Remove choosen element from the set
+// for example: exclude({1, 2, 3}, 2) == {1, 3}
+std::vector<int> exclude(std::vector<int> numbers, int number)
+{
+    auto p = std::find(std::begin(numbers), std::end(numbers), number);
+    if (p != std::end(numbers))
+        numbers.erase(p);
+    return numbers;
+}
+
+std::vector<int> ALL_DOORS = {1, 2, 3};
+
+int main()
+{
+    srand(time(0));
+    int numberOfWins = 0;
+    for (int i = 0; i < NUMBER_OF_EXPERIMENTS; ++ i)
+    {
+        int prizeDoor = chooseRandom(ALL_DOORS);
+        int choosenDoor = chooseRandom(ALL_DOORS);
+        int openedDoor = exclude(exclude(ALL_DOORS, prizeDoor), choosenDoor).front();
+        if (CHANGE_DOOR)
+            choosenDoor = exclude(exclude(ALL_DOORS, choosenDoor), openedDoor).front();
+        if (choosenDoor == prizeDoor)
+            ++ numberOfWins;
+    }
+    std::cout << numberOfWins << " / " << NUMBER_OF_EXPERIMENTS << " = " <<
+                 (static_cast<double>(numberOfWins) /
+                  static_cast<double>(NUMBER_OF_EXPERIMENTS)) << std::endl;
+    return 0;
+}
